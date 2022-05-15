6b3442b757a3677866f7694375466ef67b51491c
farid555 <farid555a@gmail.com>
2022-05-16 02:55:25 +0300

    fetch api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,19 +5,30 @@ import Hero from "../components/Hero/index";
 import CartContainer from "../components/Carts/CartContainer";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { calculateTotals } from "../features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "../features/cart/cartSlice";
 import Modal from "../components/Modal/Modal";
 
 const Home = () => {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
-  console.log(isOpen);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="fixed border items-center w-10/4 top-1/3 left-1/3 bg-red-200 rounded-md p-20 shadow-lg ">
+        <h1 className="text-xl font-bold">Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <React.Fragment>

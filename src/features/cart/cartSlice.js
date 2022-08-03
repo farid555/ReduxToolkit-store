5f469d825271a0d373eecc0e70b3c86009ec6752
farid555 <farid555a@gmail.com>
2022-08-04 02:58:06 +0300

    test

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const url = "https://course-api.com/react-useReducer-cart-project";
 
@@ -9,10 +10,13 @@ const initialState = {
   isLoading: true,
 };
 
-export const getCartItems = createAsyncThunk("cart/getCartItems", () => {
-  return fetch(url)
-    .then((resp) => resp.json())
-    .catch((err) => console.log(err));
+export const getCartItems = createAsyncThunk("cart/getCartItems", async () => {
+  try {
+    const resp = await axios(url);
+    return resp.data;
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 const cartSlice = createSlice({
@@ -21,7 +25,6 @@ const cartSlice = createSlice({
   reducers: {
     clearCart: (state) => {
       state.cartItems = [];
-      //return { cartItems: [] };
     },
 
     removeItem: (state, action) => {
